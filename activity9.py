cc4fadb3857cbdfbe0a56a4943fe4e418f5b339e
REyesBSit <gepnutzyytt@gmail.com>
2025-08-23 08:07:39 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,18 @@
+username = input("\nEnter Your Username: ")
+password = input("Enter Your Password: ")
+
+
+print("\nYour Identification is: ", (username == 'REYES') and (password == 'ITCS102') )
+
+print("\nLets play Guess a Number!!!")
+print("Guess the correct number once, and you will the prize!!!")
+
+guess1 = input("Guess First Number: ")
+guess2 = input("Guess Second Number: ")
+
+print("And the result is... ", (guess1 == '20') or (guess2 == '30'), ".... just kidding theres no prize, either way!!!")
+
+print("And the result is... ", not (guess1 == '20') or (guess2 == '30'), ".... just kidding theres no pri.... wait did I just repeated it?")
+
+
+
