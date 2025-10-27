ea62129e1c70df6cb2b44e5ca194a8b162b6ff59
REyesBSit <gepnutzyytt@gmail.com>
2025-10-27 13:38:22 +0800

    Add files via upload

---
+++ activity22.py
@@ -0,0 +1,15 @@
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+
+print(months)
+
+months.reverse()
+print(months)
+
+fullname = 'Ryan Angelo C Reyes'
+
+newlist = list(fullname)
+newlist.reverse()
+print(newlist)
+
+newlist.sort()
+print(newlist)
