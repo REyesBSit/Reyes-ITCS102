19c79cb28a3fd28698921dc52cfb9f21782fff8e
REyesBSit <gepnutzyytt@gmail.com>
2025-12-13 17:52:26 +0800

    Update and rename code_challenge1.py to code_challenge01.py

---
+++ code_challenge01.py
@@ -0,0 +1,3 @@
+name = input("Type Your Name: ")
+intro = ("Hi!!!")
+print("\t\t\t\t\t*\t\t\t\t\n\t\t\t\t*\t\t*\t\t\t\n\t\t\t*\t\t\t\t*\t\t\n\t\t*\t\t     ",intro, "\t\t\t*\t\n\t*\t\t\t     ", name,"\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\t\n\t\t\t*\t\t\t\t*\t\t\n\t\t\t\t*\t\t*\t\t\t\n\t\t\t\t\t*\t\t\t\t")
--- code_challenge1.py
+++
@@ -1,3 +0,0 @@
-name = input("Type Your Name: ")
-intro = ("Hi!!!")
-print("\t\t\t\t\t*\t\t\t\t\n\t\t\t\t*\t\t*\t\t\t\n\t\t\t*\t\t\t\t*\t\t\n\t\t*\t\t     ",intro, "\t\t\t*\t\n\t*\t\t\t     ", name,"\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\t\n\t\t\t*\t\t\t\t*\t\t\n\t\t\t\t*\t\t*\t\t\t\n\t\t\t\t\t*\t\t\t\t")
